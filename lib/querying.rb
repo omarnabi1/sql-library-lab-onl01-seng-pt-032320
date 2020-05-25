f7e26896ab159055d7a0f35c04a23812a7115dae
Omar Nabi <onabi20@gmail.com>
2020-05-25 20:01:18 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,19 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "
+   SELECT series.title FROM series
+   JOIN books
+   ON series.id = books.series_id
+   JOIN character_books
+   ON books.id = character_books.book_id
+   JOIN characters
+   ON character_books.character_id = characters.id
+   WHERE characters.species = 'human'
+   GROUP BY series.title
+   ORDER BY COUNT(*) DESC LIMIT 1;
+   
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
